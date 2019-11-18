b35cd1e2c116238c6ae823c6bbde32716848b2ee
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-18 20:41:54 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -5,8 +5,21 @@ class BankAccount
     @name = name
     @balance = 1000
     @status = 'open'
+  end
+
+  def deposit
 
   end
 
+  def display_balance
 
+  end
+
+  def valid?
+
+  end
+
+  def close_account
+
+  end
 end
