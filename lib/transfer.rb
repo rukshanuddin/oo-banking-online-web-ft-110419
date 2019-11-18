5856b53e7255ffee1c3d255e44ab9cf584ab3d56
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-11-18 20:53:39 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,11 @@
 class Transfer
   attr_accessor :status, :sender, :receiver, :amount
 
-  def initialize(sender, receiver, amount)
+  def initialize(sender, receiver, amt)
     @status = 'pending'
     @sender = sender
+    @receiver = receiver
+    @amount = amt
   end
 
 end
